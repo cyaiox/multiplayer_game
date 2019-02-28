e01bbadf77238fabf11a96284e764aca7c5a2642
Gabriel Diaz <gabrieldiaz31@gmail.com>
2019-02-28 17:52:54 -0300

    Add STATE

--- src/app.py
+++ src/app.py
@@ -1,14 +1,36 @@
+import asyncio
+import json
+
 from quart import Quart, websocket
 
 app = Quart(__name__)
+STATE = {}
+loop = asyncio.get_event_loop()
+UPDATED = asyncio.Queue(loop=loop)
 
 
-@app.route('/')
-async def hello():
+@app.route('/<int:user>')
+async def index(user):
+    global UPDATED
+    UPDATED.put_nowait(user)
     return 'hello'
+
+
+@app.route('/update/<int:user>')
+async def update(user):
+    global UPDATED
+    UPDATED.put_nowait(user)
+    return 'udpated!!'
 
 
 @app.websocket('/ws')
 async def ws():
+    global UPDATED
     while True:
-        await websocket.send('hello')
+        await UPDATED.get()
+        await websocket.send(json.dumps({'type': 'state', **STATE}))
+        UPDATED.task_done()
+
+
+if __name__ == "__main__":
+    app.run(loop=loop, host='0.0.0.0', port='5000')
